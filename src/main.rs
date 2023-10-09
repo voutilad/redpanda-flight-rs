76b9ae02bd93b5e47931ff123eb6e994a1123152
Dave Voutila <voutilad@gmail.com>
2023-10-09 15:11:45 -0400

    skeleton of flight service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+mod server;
+
+use std::env;
+
+use tracing::info;
+use tracing_subscriber;
+
+use tonic::transport::Server;
+use arrow_flight::flight_service_server::FlightServiceServer;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
+
+    tracing_subscriber::fmt::init();
+
+    let addr = "127.0.0.1:9999".parse().unwrap();
+    let rpfs = server::RedpandaFlightService{};
+
+    let svc = FlightServiceServer::new(rpfs);
+
+    info!("starting with address {}", addr);
+    Server::builder().add_service(svc).serve(addr).await?;
+
+    info!("bye!");
+    Ok(())
 }
---
+++ src/server.rs
@@ -0,0 +1,96 @@
+use tracing::warn;
+
+use futures::stream::BoxStream;
+use tonic::{Request, Response, Status, Streaming};
+
+use arrow_flight::flight_service_server::FlightService;
+use arrow_flight::{
+    Action, ActionType, Criteria, Empty, FlightData, FlightDescriptor, FlightInfo,
+    HandshakeRequest, HandshakeResponse, PutResult, SchemaResult, Ticket,
+};
+
+#[derive(Clone)]
+pub struct RedpandaFlightService {}
+
+#[tonic::async_trait]
+impl FlightService for RedpandaFlightService {
+    type HandshakeStream = BoxStream<'static, Result<HandshakeResponse, Status>>;
+    type ListFlightsStream = BoxStream<'static, Result<FlightInfo, Status>>;
+    type DoGetStream = BoxStream<'static, Result<FlightData, Status>>;
+    type DoPutStream = BoxStream<'static, Result<PutResult, Status>>;
+    type DoActionStream = BoxStream<'static, Result<arrow_flight::Result, Status>>;
+    type ListActionsStream = BoxStream<'static, Result<ActionType, Status>>;
+    type DoExchangeStream = BoxStream<'static, Result<FlightData, Status>>;
+
+    async fn handshake(
+        &self,
+        _request: Request<Streaming<HandshakeRequest>>,
+    ) -> Result<Response<Self::HandshakeStream>, Status> {
+        warn!("handshake not implemented");
+        Err(Status::unimplemented("Implement handshake"))
+    }
+
+    async fn list_flights(
+        &self,
+        _request: Request<Criteria>,
+    ) -> Result<Response<Self::ListFlightsStream>, Status> {
+        warn!("list_flights not implemented");
+        Err(Status::unimplemented("Implement list_flights"))
+    }
+
+    async fn get_flight_info(
+        &self,
+        _request: Request<FlightDescriptor>,
+    ) -> Result<Response<FlightInfo>, Status> {
+        warn!("get_flight_info not implemented");
+        Err(Status::unimplemented("Implement get_flight_info"))
+    }
+
+    async fn get_schema(
+        &self,
+        _request: Request<FlightDescriptor>,
+    ) -> Result<Response<SchemaResult>, Status> {
+        warn!("get_schema not implemented");
+        Err(Status::unimplemented("Implement get_schema"))
+    }
+
+    async fn do_get(
+        &self,
+        _request: Request<Ticket>,
+    ) -> Result<Response<Self::DoGetStream>, Status> {
+        warn!("do_get not implemented");
+        Err(Status::unimplemented("Implement do_get"))
+    }
+
+    async fn do_put(
+        &self,
+        _request: Request<Streaming<FlightData>>,
+    ) -> Result<Response<Self::DoPutStream>, Status> {
+        warn!("do_put not implemented");
+        Err(Status::unimplemented("Implement do_put"))
+    }
+
+    async fn do_action(
+        &self,
+        _request: Request<Action>,
+    ) -> Result<Response<Self::DoActionStream>, Status> {
+        warn!("do_action not implemented");
+        Err(Status::unimplemented("Implement do_action"))
+    }
+
+    async fn list_actions(
+        &self,
+        _request: Request<Empty>,
+    ) -> Result<Response<Self::ListActionsStream>, Status> {
+        warn!("list_actions not implemented");
+        Err(Status::unimplemented("Implement list_actions"))
+    }
+
+    async fn do_exchange(
+        &self,
+        _request: Request<Streaming<FlightData>>,
+    ) -> Result<Response<Self::DoExchangeStream>, Status> {
+        warn!("do_exchange not implemented");
+        Err(Status::unimplemented("Implement do_exchange"))
+    }
+}
