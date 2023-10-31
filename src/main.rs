1e2fddbdf3ed7c551219bc291a27a75db7e948b7
Dave Voutila <voutilad@gmail.com>
2023-10-31 12:58:20 -0400

    initial support for tls front-end

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::env;
+use std::path::Path;
 use std::process::ExitCode;
 
 use arrow_flight::flight_service_server::FlightServiceServer;
-use tonic::transport::Server;
-use tracing::{info, warn};
+use tonic::transport::{Identity, Server, ServerTlsConfig};
+use tracing::{error, info, warn};
 use tracing_subscriber;
 
 use crate::redpanda::{Auth, AuthMechanism, AuthProtocol};
@@ -13,6 +14,11 @@ mod redpanda;
 mod registry;
 mod schema;
 mod server;
+
+// These are borrowed from Redpanda's docs to try to simplify alignment.
+// See https://docs.redpanda.com/current/manage/security/encryption/
+const DEFAULT_BROKER_KEY: &str = "broker.key";
+const DEFAULT_BROKER_CERT: &str = "broker.crt";
 
 #[tokio::main]
 async fn main() -> ExitCode {
@@ -29,6 +35,10 @@ async fn main() -> ExitCode {
     let env_password = env::var("REDPANDA_SASL_PASSWORD");
     let env_mechanism = env::var("REDPANDA_SASL_MECHANISM");
     let use_tls = env::var("REDPANDA_TLS_ENABLED").is_ok();
+    let env_cert = env::var("REDPANDA_FLIGHT_CERT").unwrap_or(String::from(DEFAULT_BROKER_CERT));
+    let env_key = env::var("REDPANDA_FLIGHT_KEY").unwrap_or(String::from(DEFAULT_BROKER_KEY));
+    let tls_cert = Path::new(&env_cert);
+    let tls_key = Path::new(&env_key);
 
     // Piece together our authentication details for our backend/admin connection.
     let auth: Option<Auth> = match env_username {
@@ -87,19 +97,42 @@ async fn main() -> ExitCode {
         .unwrap();
     info!("listening on {}", addr);
 
+    // XXX We require TLS on the front-end if we're using it on the back-end to prevent security
+    // footguns. To prevent the illusion of security, we expect both sides to be using TLS or not.
+    let tls_config: Option<ServerTlsConfig> = match use_tls {
+        true => {
+            // TODO: graceful error handling
+            let cert = tokio::fs::read(tls_cert).await.unwrap();
+            let key = tokio::fs::read(tls_key).await.unwrap();
+            Some(ServerTlsConfig::new().identity(Identity::from_pem(cert, key)))
+        }
+        false => {
+            if auth.is_some() {
+                warn!("authentication provided, but no TLS configured for front-end");
+                return ExitCode::FAILURE;
+            }
+            None
+        }
+    };
+
     let redpanda = server::RedpandaFlightService::new(seeds.as_str(), topic.as_str(), auth).await;
     if redpanda.is_err() {
         return ExitCode::FAILURE;
     }
     let svc = FlightServiceServer::new(redpanda.unwrap());
 
-    // Launch the flight service. This only resolves when we're shutting down.
-    if Server::builder()
-        .add_service(svc)
-        .serve(addr)
-        .await
-        .is_err()
-    {
+    // Launch the flight service.
+    let mut builder = Server::builder();
+    if tls_config.is_some() {
+        builder = match builder.tls_config(tls_config.unwrap()) {
+            Ok(s) => s,
+            Err(e) => {
+                error!("failed to initialize tls config: {}", e);
+                return ExitCode::FAILURE;
+            }
+        };
+    }
+    if builder.add_service(svc).serve(addr).await.is_err() {
         return ExitCode::FAILURE;
     }
 
